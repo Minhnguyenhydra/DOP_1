6fb1e81c347a5bb98a403a201cc01d26d93c85ae
Duchuy <huy.hd@rubycell.com>
2022-11-02 13:50:48 +0700

    change id to test id

--- Assets/GoogleAdMobController.cs
+++ Assets/GoogleAdMobController.cs
@@ -9,10 +9,15 @@ using System.Collections.Generic;
 public class GoogleAdMobController : MonoBehaviour {
     public static GoogleAdMobController Instance;
 
-    public const string ADD_ID_BANNER = "ca-app-pub-9219376842089406/8582091893";
-    public const string ADD_ID_REWARDED = "ca-app-pub-9219376842089406/6738543095";
-    public const string ADD_ID_APP_OPEN = "ca-app-pub-9219376842089406/8361727903";
-    public const string ADD_ID_INTERTISTIAL = "ca-app-pub-9219376842089406/7668481380";
+    //public const string ADD_ID_BANNER = "ca-app-pub-9219376842089406/8582091893";
+    //public const string ADD_ID_REWARDED = "ca-app-pub-9219376842089406/6738543095";
+    //public const string ADD_ID_APP_OPEN = "ca-app-pub-9219376842089406/8361727903";
+    //public const string ADD_ID_INTERTISTIAL = "ca-app-pub-9219376842089406/7668481380";    
+    
+    public const string ADD_ID_BANNER = "ca-app-pub-3940256099942544/6300978111";
+    public const string ADD_ID_REWARDED = "ca-app-pub-3940256099942544/5224354917";
+    public const string ADD_ID_APP_OPEN = "ca-app-pub-3940256099942544/3419835294";
+    public const string ADD_ID_INTERTISTIAL = "ca-app-pub-3940256099942544/1033173712";
 
     //config
     private readonly AdPosition BANNER_POSITION = AdPosition.Bottom;
